b90f85bc2de0626cf264f223581e5d943076d5f5
Rafael <rafaelmagalhaes1@live.com>
2023-09-16 21:06:10 -0300

    feat: requisito 4

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,51 +1,80 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import '../App.css';
+import './home.css';
 import iconCart from '../images/icon-shopping-cart.png';
+import { getCategories } from '../services/api';
 
 function Home() {
   const [search, setSearch] = useState('');
+  const [categories, setCategories] = useState([]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
 
+  useEffect(() => {
+    const fetchApiCategories = async () => {
+      const categorie = await getCategories();
+      setCategories(categorie);
+    };
+    fetchApiCategories();
+  }, []);
+
   return (
     <main>
-      <nav>
-        <form className="search-form">
-          <input
-            className="search-input"
-            type="text"
-            name="search"
-            value={ search }
-            onChange={ handleChange }
-          />
-        </form>
+      <aside className="search-categories">
+        <h2>Categorias</h2>
+        <div>
+          <label data-testid="category" htmlFor="categoria1"> Categoria 1</label>
+          <input type="radio" id="categoria1" />
+        </div>
 
-        <Link
-          to="/shoppingcart"
-          className="button-edit-profile"
-        >
-          <img
-            data-testid="shopping-cart-button"
-            className="icon-shopping-cart"
-            src={ iconCart }
-            alt="Link Shopping Cart"
-          />
-        </Link>
-      </nav>
+        <div>
+          <label data-testid="category" htmlFor="categoria1"> Categoria 2</label>
+          <input type="radio" id="categoria2" />
+        </div>
 
-      <div className="search-result">
-        {search ? (
-          <h2>Resultado</h2>
-        ) : (
-          <p className="initial-message" data-testid="home-initial-message">
-            Digite algum termo de pesquisa ou escolha uma categoria.
-          </p>
-        )}
-      </div>
+        <div>
+          <label data-testid="category" htmlFor="categoria1"> Categoria 3</label>
+          <input type="radio" id="categoria3" />
+        </div>
+      </aside>
 
+      <section>
+        <div className="search-form">
+          <form>
+            <input
+              className="search-input"
+              type="text"
+              name="search"
+              value={ search }
+              onChange={ handleChange }
+            />
+          </form>
+
+          <Link
+            to="/shoppingcart"
+            className="button-edit-profile"
+          >
+            <img
+              data-testid=""
+              className="icon-shopping-cart"
+              src={ iconCart }
+              alt="Link Shopping Cart"
+            />
+          </Link>
+        </div>
+
+        <div className="search-result">
+          {search ? (
+            <h2>Resultado</h2>
+          ) : (
+            <p className="initial-message" data-testid="home-initial-message">
+              Digite algum termo de pesquisa ou escolha uma categoria.
+            </p>
+          )}
+        </div>
+      </section>
     </main>
   );
 }
