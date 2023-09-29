bc0d293976bd65c6af0de50ba2055d14860a4997
Rafael <rafaelmagalhaes1@live.com>
2023-09-29 12:58:35 -0300

    create GlobalStyles

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './redux';
-import './index.css';
+import { GlobalStyle } from './GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <BrowserRouter>
     <Provider store={ store }>
+      <GlobalStyle />
       <App />
     </Provider>
   </BrowserRouter>,
